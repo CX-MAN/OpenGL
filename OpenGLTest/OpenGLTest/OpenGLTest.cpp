db01f237afcdf047403ee51371b37a4bcc9ecccd
CX-MAN <506208690@qq.com>
2018-05-31 10:10:29 +0800

    添加opengl测试代码

--- OpenGLTest/OpenGLTest/OpenGLTest.cpp
+++ OpenGLTest/OpenGLTest/OpenGLTest.cpp
@@ -1,12 +1,39 @@
 // OpenGLTest.cpp: 主项目文件。
 
 #include "stdafx.h"
-
+#include<stdlib.h>
+#include"glut.h"
 using namespace System;
-
-int main(array<System::String ^> ^args)
+void init(void)
 {
-    Console::WriteLine(L"Hello World");
-    return 0;
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void lineSegment(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex2i(180, 15);
+	glVertex2i(10, 145);
+	glEnd();
+
+	glFlush();
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(50, 100);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("Demo OpenGL Program");
+
+	init();
+	glutDisplayFunc(lineSegment);
+	glutMainLoop();
 
 }
