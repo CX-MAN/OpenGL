fb3ad8a5b36ef2f2321d88b519c7d1528cdeb5dd
CX-MAN <506208690@qq.com>
2018-06-02 20:08:52 +0800

    绘制矩形

--- OpenGLTest/OpenGLTest/OpenGLTest.cpp
+++ OpenGLTest/OpenGLTest/OpenGLTest.cpp
@@ -3,36 +3,35 @@
 #include "stdafx.h"
 #include<stdlib.h>
 #include"glut.h"
-void init(void)
-{
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
-}
-
-void lineSegment(void)
+void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-	glVertex2i(180, 15);
-	glVertex2i(10, 145);
+	glColor3f(0, 1.0, 0);
+	glBegin(GL_POLYGON);
+	glVertex3f(0.25, 0.25, 0);
+	glVertex3f(0.75, 0.25, 0);
+	glVertex3f(0.75, 0.75, 0);
+	glVertex3f(0.25, 0.75, 0);
 	glEnd();
-
 	glFlush();
 }
-
+void init()
+{
+	glClearColor(0.0, 0.0, 0.0,0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("Demo OpenGL Program");
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(100, 100);
+	glutCreateWindow("Hello");
 
 	init();
-	glutDisplayFunc(lineSegment);
+	glutDisplayFunc(display);
 	glutMainLoop();
 
 }
